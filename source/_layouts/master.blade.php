851e11ec8465e91b4024771c33013d11eadb13e1
Chris White <cwhite@intouchinsight.com>
2020-02-10 21:48:48 -0500

    more styling updates

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -8,7 +8,7 @@
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Source+Sans+Pro:400,400i,700,700i&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body class="font-body py-10 bg-white min-h-screen">
+    <body class="font-body text-lg py-10 bg-white min-h-screen">
         <div class="w-full h-1 bg-red-600 fixed top-0"></div>
 
         <div class="container mx-auto">
