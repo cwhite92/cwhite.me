0e5de2be224ea47bc340691663e385b47ed48ba0
Chris White <cwhite@intouchinsight.com>
2020-02-11 19:58:22 -0500

    nav bar

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -8,8 +8,16 @@
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Source+Sans+Pro:400,400i,700,700i|Source+Code+Pro&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body class="font-body text-lg py-10 bg-white min-h-screen">
+    <body class="font-body text-lg bg-white min-h-screen">
         <div class="w-full h-1 bg-red-600 fixed top-0"></div>
+        <div class="border-b border-gray-300 mb-6">
+            <div class="container mx-auto">
+                <a href="/" class="inline-block p-4 -ml-4 text-black border-none">Home</a>
+                <a href="/blog" class="inline-block p-4 text-black border-none">Blog</a>
+                <a href="/about" class="inline-block p-4 text-black border-none">About</a>
+                <a href="/contact" class="inline-block p-4 text-black border-none">Contact</a>
+            </div>
+        </div>
 
         <div class="container mx-auto">
             @yield('body')
