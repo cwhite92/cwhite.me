0c6199ff0ceab418e86ca21217521fae049a99bb
Chris White <cwhite@intouchinsight.com>
2020-02-11 20:09:11 -0500

    mvoe blog posts list to /blog

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     @foreach ($posts as $post)
         <div class="relative">
-            <h1><a href="{{ $post->getUrl() }}">{{ $post->title }}</a></h1>
+            <h1><a class="text-black border-none" href="{{ $post->getUrl() }}">{{ $post->title }}</a></h1>
             <p>{!! $post->excerpt() !!}</p>
             <div class="fade"></div>
         </div>
