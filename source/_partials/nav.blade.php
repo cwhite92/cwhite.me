d6d4bedcf7830db83ce8e0b0258aa0751e7d9a29
Chris White <cwhite@intouchinsight.com>
2020-02-16 12:10:44 -0500

    contact page

--- source/_partials/nav.blade.php
+++ source/_partials/nav.blade.php
@@ -2,7 +2,6 @@
     <div class="container mx-auto">
         <a href="/" class="inline-block p-4 -ml-4 text-black border-none">Home</a>
         <a href="/blog" class="inline-block p-4 text-black border-none">Blog</a>
-        <a href="/about" class="inline-block p-4 text-black border-none">About</a>
         <a href="/contact" class="inline-block p-4 text-black border-none">Contact</a>
     </div>
 </div>
---
+++ source/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+    @include('_partials.nav')
+
+    <div class="container mx-auto">
+        <h1>Contact</h1>
+        <p>Feel free to reach out on any of the methods below!</p>
+
+        <p>Email: <a href="mailto:chris@cwhite.me">chris@cwhite.me</a></p>
+        <p>Twitter: <a href="https://twitter.com/cwhite_92">@cwhite_92</a></p>
+    </div>
+
+@endsection
