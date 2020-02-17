fade079d87ec3f3c78d29d574f800835791e3c63
Chris White <cwhite@intouchinsight.com>
2020-02-17 11:42:31 -0500

    dont show post separator if last post

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -20,12 +20,14 @@ pagination:
                 <div class="fade"></div>
             </div>
 
-            <div class="text-center my-32">
-                <div class="p-2 bg-white inline-block">
-                    <svg class="text-gray-500 fill-current" width="24" height="24"><path d="M18.4 8.5l1.4 1.4L7.1 22.6 0 24l1.4-7.1L14.1 4.2l1.4 1.4L3.3 18l-.7 3.5 3.5-.7L18.4 8.5zm0-8.5l-3 2.8 5.8 5.7L24 5.7 18.3 0zM6 18.7L17.3 7.4l-.7-.7L5.3 18l.7.7z"/></svg>
+            @if (!$loop->last)
+                <div class="text-center my-32">
+                    <div class="p-2 bg-white inline-block">
+                        <svg class="text-gray-500 fill-current" width="24" height="24"><path d="M18.4 8.5l1.4 1.4L7.1 22.6 0 24l1.4-7.1L14.1 4.2l1.4 1.4L3.3 18l-.7 3.5 3.5-.7L18.4 8.5zm0-8.5l-3 2.8 5.8 5.7L24 5.7 18.3 0zM6 18.7L17.3 7.4l-.7-.7L5.3 18l.7.7z"/></svg>
+                    </div>
+                    <div class="h-px w-full bg-gray-300" style="margin-top: -26px;"></div>
                 </div>
-                <div class="h-px w-full bg-gray-300" style="margin-top: -26px;"></div>
-            </div>
+            @endif
         @endforeach
 
         <div class="flex pb-12">
